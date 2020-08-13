6b4a89634302eabd3882a0a7cd408c4ec61f1093
RofixWork <wourkout123@gmail.com>
2020-08-13 15:22:01 +0100

    Updates

---
+++ precache-manifest.278be18a5a6b508f5432dc188ee55668.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72f8294930ec79f87f801dbb571836b5",
+    "url": "/LandingPage/index.html"
+  },
+  {
+    "revision": "d05d4dcfb0c59ebad30f",
+    "url": "/LandingPage/static/css/2.c3594674.chunk.css"
+  },
+  {
+    "revision": "39fb841764621092c326",
+    "url": "/LandingPage/static/css/main.ddcd8dc1.chunk.css"
+  },
+  {
+    "revision": "d05d4dcfb0c59ebad30f",
+    "url": "/LandingPage/static/js/2.d06269c2.chunk.js"
+  },
+  {
+    "revision": "24b8054ddc1445462862104fe5fc876e",
+    "url": "/LandingPage/static/js/2.d06269c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39fb841764621092c326",
+    "url": "/LandingPage/static/js/main.67c74751.chunk.js"
+  },
+  {
+    "revision": "b069c2dcb5e2ad623880",
+    "url": "/LandingPage/static/js/runtime-main.7c331e65.js"
+  },
+  {
+    "revision": "00d7213c62b126a0de40056376cf924e",
+    "url": "/LandingPage/static/media/1.00d7213c.jpg"
+  },
+  {
+    "revision": "229156906a975a18015a0183a32f86ad",
+    "url": "/LandingPage/static/media/1.22915690.jpg"
+  },
+  {
+    "revision": "140aab7dbce0a9daf3595d83e828f2f0",
+    "url": "/LandingPage/static/media/2.140aab7d.jpg"
+  },
+  {
+    "revision": "31387364efd5048127621a501e3fa87c",
+    "url": "/LandingPage/static/media/2.31387364.jpg"
+  },
+  {
+    "revision": "78e6fbeb4049c6f49ef3c7d8b455519b",
+    "url": "/LandingPage/static/media/3.78e6fbeb.jpg"
+  },
+  {
+    "revision": "ec4ad7c5c25e66268bf0c64cc23a05c2",
+    "url": "/LandingPage/static/media/3.ec4ad7c5.jpg"
+  },
+  {
+    "revision": "a348aad4a3aae14f32a9603710480825",
+    "url": "/LandingPage/static/media/4.a348aad4.jpg"
+  },
+  {
+    "revision": "140aab7dbce0a9daf3595d83e828f2f0",
+    "url": "/LandingPage/static/media/5.140aab7d.jpg"
+  },
+  {
+    "revision": "395a0a2a9dd1f4e5e119af660aee0cf4",
+    "url": "/LandingPage/static/media/watch-01.395a0a2a.png"
+  },
+  {
+    "revision": "6323c9cbe8355fc57f3d25554ee9f9aa",
+    "url": "/LandingPage/static/media/watch-02.6323c9cb.jpg"
+  }
+]);
